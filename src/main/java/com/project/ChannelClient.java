3b154a7cfd0699924cabb5b22637bbb462b75df3
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-06-07 15:49:54 +0400

    uber tchannel

---
+++ src/main/java/com/project/ChannelClient.java
@@ -0,0 +1,74 @@
+package com.project;
+
+import com.uber.tchannel.api.SubChannel;
+import com.uber.tchannel.api.TChannel;
+import com.uber.tchannel.api.TFuture;
+import com.uber.tchannel.api.handlers.TFutureCallback;
+import com.uber.tchannel.messages.RawRequest;
+import com.uber.tchannel.messages.RawResponse;
+import java.net.InetAddress;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ *
+ * @author armdev
+ */
+public class ChannelClient {
+
+    public static void main(String[] args) {
+        try {
+            TChannel client = createClient();
+
+            SubChannel subChannel = client.makeSubChannel("server");
+
+            final long start = System.currentTimeMillis();
+            final CountDownLatch done = new CountDownLatch(3);
+
+            TFutureCallback<RawResponse> callback = (RawResponse response) -> {
+                // when using callback, resource associated with response is released by the the TChannel library
+                if (!response.isError()) {
+                    System.out.println(String.format("Response received: response code: %s, header: %s, body: %s",
+                            response.getResponseCode(),
+                            response.getHeader(),
+                            response.getBody()));
+                } else {
+                    System.out.println(String.format("Got error response: %s",
+                            response.toString()));
+                }
+
+                done.countDown();
+            };
+
+            // send three requests
+            for (int i = 0; i < 10; i++) {
+                RawRequest request = new RawRequest.Builder("server", "pong")
+                        .setHeader("Marco")
+                        .setBody("Ping!")
+                        .build();
+                TFuture<RawResponse> future = subChannel.send(request,
+                        InetAddress.getByName("127.0.0.1"),
+                        8888
+                );
+                future.addCallback(callback);
+            }
+            done.await();
+            System.out.println(String.format("\nTime cost: %dms", System.currentTimeMillis() - start));
+            client.shutdown(false);
+        } catch (Exception ex) {
+            ex.getLocalizedMessage();
+        }
+
+    }
+
+    protected static TChannel createClient() throws Exception {
+
+        // create TChannel
+        TChannel tchannel = new TChannel.Builder("client")
+                .build();
+
+        // create sub channel to talk to server
+        tchannel.makeSubChannel("server");
+        return tchannel;
+    }
+
+}
---
+++ src/main/java/com/project/ChannelServer.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.project;
+
+import com.uber.tchannel.api.ResponseCode;
+import com.uber.tchannel.api.TChannel;
+import com.uber.tchannel.api.handlers.RawRequestHandler;
+import com.uber.tchannel.messages.RawRequest;
+import com.uber.tchannel.messages.RawResponse;
+import java.net.InetAddress;
+
+/**
+ *
+ * @author armenar
+ */
+public class ChannelServer {
+
+    public static void main(String[] args) {
+        try {
+            TChannel server = createServer();
+        } catch (Exception ex) {
+            ex.getLocalizedMessage();
+        }
+    }
+
+    private static TChannel createServer() throws Exception {
+        // create TChannel
+        TChannel tchannel = new TChannel.Builder("server")
+                .setServerHost(InetAddress.getByName("127.0.0.1"))
+                .setServerPort(8888)
+                .build();
+        System.out.println("Server started");
+        tchannel.makeSubChannel("server")
+                .register("pong", new RawRequestHandler() {
+                    private int count = 0;
+                    @Override
+                    public RawResponse handleImpl(RawRequest request) {
+                        System.out.println(String.format("Request received: header: %s, body: %s",
+                                request.getHeader(),
+                                request.getBody()));
+                        count++;
+                        switch (count) {
+                            case 1:
+                                return new RawResponse.Builder(request)
+                                        .setTransportHeaders(request.getTransportHeaders())
+                                        .setHeader("Polo")
+                                        .setBody("Pong!")
+                                        .build();
+                            case 2:
+                                return new RawResponse.Builder(request)
+                                        .setTransportHeaders(request.getTransportHeaders())
+                                        .setResponseCode(ResponseCode.Error)
+                                        .setHeader("Polo")
+                                        .setBody("I feel bad ...")
+                                        .build();
+                            default:
+                                throw new UnsupportedOperationException("I feel very bad!");
+                        }
+                    }
+                });
+        tchannel.listen();
+        return tchannel;
+    }
+
+}
